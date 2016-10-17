b9ceb8475b12fa3f0dbac44a27a998cf925dbe83
JakubBadowski <jakub.badowski@gmail.com>
2016-10-17 18:35:23 +0000

    very first example

---
+++ migacz.py
@@ -0,0 +1,23 @@
+import time
+import RPi.GPIO as gpio
+
+print('Prosty program migacz')
+
+gpio.setmode(gpio.BCM)
+
+gpio.setwarnings(False)
+
+gpio.setup(26,gpio.OUT)
+# GPIO.setup(3,GPIO.OUT)
+
+interwal = 3
+
+while True:
+	print('ON')
+	gpio.output(26,gpio.HIGH)
+	time.sleep(interwal)
+
+	print('OFF')
+	gpio.output(26,gpio.LOW)
+	time.sleep(interwal)
+
